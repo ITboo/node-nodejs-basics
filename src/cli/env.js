3405fd82894e897cc56a541333665a4e63a825ae
ITboo <annaacdc@tut.by>
2024-01-14 19:11:37 +0300

    feat: add cli subtask solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    const separator = '; ';
+
+    const variables = Object.entries(process.env).filter(([key]) => key.startsWith(prefix));
+    const consoleOutput = variables.map((variable) => variable.join('=')).join(separator);
+
+    console.log(consoleOutput);
 };
 
 parseEnv();
