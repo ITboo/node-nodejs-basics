1c8c3f31894c6b2c2463d939c7dd160b97943edb
ITboo <annaacdc@tut.by>
2024-01-16 20:44:08 +0300

    feat: implement hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+
+import { getPath } from '../common/helpers.js';
+import { FS_ERROR_MSG } from '../common/constants.js';
+
+const name = 'fileToCalculateHashFor.txt';
+const path = getPath(import.meta.url, 'files', name);
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const buffer = await readFile(path);
+    const hash = createHash('sha256').update(buffer).digest('hex');
+    console.log("Hash: ", hash);
+  } catch {
+    throw new Error(FS_ERROR_MSG);
+  }
 };
 
 await calculateHash();
